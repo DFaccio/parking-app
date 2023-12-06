4b945279f117147451a67ee443e7dea45a7eadda
Danilo Faccio <facciodrums@gmail.com>
2023-12-06 20:38:31 -0300

    primeiro commit para criação do projeto

---
+++ src/main/java/br/com/gramadoparking/gramadoparkingapp/GramadoparkingAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.gramadoparking.gramadoparkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramadoparkingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramadoparkingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gramadoparking/gramadoparkingapp/GramadoparkingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gramadoparking.gramadoparkingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramadoparkingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
