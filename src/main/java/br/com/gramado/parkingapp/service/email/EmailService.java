0f2494c5b88c3900e3c63e352311be9cf8f0e762
Aydan Amorim <aydanamorim@gmail.com>
2024-01-27 18:32:49 -0300

    Atualizacao do Patch com classes pertinentes ao envio de email

---
+++ src/main/java/br/com/gramado/parkingapp/service/email/EmailService.java
@@ -0,0 +1,37 @@
+package br.com.gramado.parkingapp.service.email;
+
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+class EmailService implements EmailServiceInterface {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+
+    @Resource
+    private JavaMailSender javaMailSender;
+
+    private static final String parkingApp = "Parking App - ";
+
+    public void sendEmail(String email, String subject, String message) {
+        String title = parkingApp + subject;
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(email);
+        mailMessage.setSubject(title);
+        mailMessage.setText(message);
+
+        try {
+            javaMailSender.send(mailMessage);
+
+            LOGGER.info(title + ": e-mail enviado para " + email);
+        } catch (MailException exception) {
+            LOGGER.error("Error on send e-mail to " + email, exception);
+        }
+    }
+}
